eb700a84b37141e38ff6c613c5b3f297ff66bd43
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-20 17:51:15 -0300

    added log statement to debug

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var dbConnection = require("../utils/database");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(dbConnection.get("message"));
+    console.log(dbConnection.db.collection("message").find());
     res.render('index', { data: dbConnection.get("message") });
 });
 
