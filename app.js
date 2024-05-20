0cf142e3d923ce4fa5514a3fb94234b7107987d9
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-20 12:01:14 -0300

    moved location of connection script

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const {connectDB} = require("./utils/database");
+const config = require("./utils/config");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,7 +14,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-connectDB().then(() => {console.log("tuki")});
+let mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
+
+try {
+    await mongoose.connect(mongoUri);
+} catch (err) {
+    console.log(err.message);
+    process.exit(1);
+}
+
+const dbConnection = mongoose.connection;
+
+dbConnection.once("open", () => {
+    console.log(`Database connected: ${mongoUri}`);
+});
+
+dbConnection.on("error", (err) => {
+    console.error(`Connection error: ${err}`);
+});
+
+await dbConnection.createCollection("works");
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
