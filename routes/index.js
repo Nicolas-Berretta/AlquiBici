48a143a376d3dd6d8010e3ab74c3ed1b2df74a4b
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-20 18:08:45 -0300

    modified index.ejs

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var database = require("../utils/database");
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    console.log(await database.collection("message").findOne());
-    res.render('index', { data: await database.collection("message").findOne() });
+    console.log(await database.collection("message").find().toArray());
+    res.render('index', { data: await database.collection("message").find().toArray()});
 });
 
 module.exports = router;
