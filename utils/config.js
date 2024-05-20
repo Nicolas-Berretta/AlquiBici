128fec146e4a1644032aec6bdd3adcc19ce44066
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-20 11:30:43 -0300

    changed MONGODB_HOSTNAME

--- utils/config.js
+++ utils/config.js
@@ -8,7 +8,7 @@ config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '172.31.86.248'; // Cambia
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
-config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || '100.27.200.249'; // Cambiar
+config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || '127.0.0.1'; // Cambiar
 config.mongodb.port       = process.env.MONGODB_PORT       || 27017;
 config.mongodb.database   = process.env.MONGODB_DATABASE   || 'test';
 config.mongodb.collection = process.env.MONGODB_COLLECTION || 'message';
