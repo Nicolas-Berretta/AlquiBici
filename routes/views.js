722617408c24a087d7e0d1d9487407859053a734
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-24 13:20:42 -0300

    removed thing

--- routes/views.js
+++ routes/views.js
@@ -5,7 +5,6 @@ var database = require('../utils/database');
 /* GET home page. */ // TODO have routes to display views and equivalent ones to send data to the frontend
 router.get('/', async function(req, res, next) {
     console.log(await database.collection("message").find().toArray());
-    res.status(200).send("hey");
     res.render('index', { data: await database.collection("message").find().toArray()});
 });
 
