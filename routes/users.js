b206f817ae3801e5d373ca5ee6970c874337de85
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-27 09:11:28 -0300

    fixed profileService

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerService, loginService, profileService } = require('../services/users');
+const { registerService, loginService, getUserService} = require('../services/users');
 
 router.post('/register', registerService);
 
 router.post('/login', loginService);
 
-router.get('/profile/:email', profileService);
+router.get('/get/:email', getUserService);
 
 module.exports = router;
