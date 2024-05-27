533433eb4fc83a400effc100183ff580b54916be
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-27 09:52:51 -0300

    sjo;idfcjbewhtlrqehldigawlfhwkli

--- services/users.js
+++ services/users.js
@@ -35,6 +35,6 @@ exports.loginService = async (req, res) => {
 exports.getUserService = async (req, res) => {
     let email = req.params.email;
     console.log(email);
-    let user = getUserByEmail(email).catch(e => {return res.status(500).send(e)});
+    let user = await getUserByEmail(email).catch(e => {return res.status(500).send(e)});
     return res.status(200).send(user);
 }
